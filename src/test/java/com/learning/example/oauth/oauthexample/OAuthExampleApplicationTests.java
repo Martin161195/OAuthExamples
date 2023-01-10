b3f98cfbf087e5bc192e7f80db0021bd48f4e5b0
Romulo Martin Ramirez Pando <rramirpa@emeal.nttdata.com>
2023-01-10 11:08:28 -0500

    initial commit

---
+++ src/test/java/com/learning/example/oauth/oauthexample/OAuthExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.example.oauth.oauthexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
